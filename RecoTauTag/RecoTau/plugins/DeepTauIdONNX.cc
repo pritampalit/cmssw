5f1f76489483437db03cc09b2c12ea5334a3e632
PRITAM PALIT <pritampalit@gmail.com>
2026-07-08 15:30:23 +0200

    fixes for score mismatch

--- RecoTauTag/RecoTau/plugins/DeepTauIdONNX.cc
+++ RecoTauTag/RecoTau/plugins/DeepTauIdONNX.cc
@@ -12,27 +12,27 @@
 #include <fstream>
 
 namespace deep_tau {
-
-class DeepTauCacheONNX {
-public:
-  DeepTauCacheONNX(const std::map<std::string, std::string>& graph_names) {
-    for (const auto& entry : graph_names)
-      sessions_[entry.first] = std::make_unique<cms::Ort::ONNXRuntime>(entry.second);
-  }
-
-  const cms::Ort::ONNXRuntime& getSession(const std::string& name = "") const {
-    return *sessions_.at(name);
-  }
-
-private:
-  std::map<std::string, std::unique_ptr<cms::Ort::ONNXRuntime>> sessions_;
-};
-
+  
+  class DeepTauCacheONNX {
+  public:
+    DeepTauCacheONNX(const std::map<std::string, std::string>& graph_names) {
+      for (const auto& entry : graph_names)
+	sessions_[entry.first] = std::make_unique<cms::Ort::ONNXRuntime>(entry.second);
+    }
+    
+    const cms::Ort::ONNXRuntime& getSession(const std::string& name = "") const {
+      return *sessions_.at(name);
+    }
+    
+  private:
+    std::map<std::string, std::unique_ptr<cms::Ort::ONNXRuntime>> sessions_;
+  };
+  
 } // namespace deep_tau
 
 
 class DeepTauIdONNXWrapper
-    : public edm::stream::EDProducer<edm::GlobalCache<deep_tau::DeepTauCacheONNX>> {
+  : public edm::stream::EDProducer<edm::GlobalCache<deep_tau::DeepTauCacheONNX>> {
 public:
   explicit DeepTauIdONNXWrapper(const edm::ParameterSet&) {}
 };
@@ -40,52 +40,76 @@ public:
 
 class DeepTauIdONNX : public DeepTauIdBaseONNX<DeepTauIdONNXWrapper> {
 public:
-
+  
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     edm::ParameterSetDescription desc;
     fillDescriptionsHelper(desc);
     desc.add<std::vector<std::string>>("graph_file",
 				       //{});
-        {"/afs/cern.ch/work/p/ppalit2/public/test_sonic/CMSSW_16_0_0_pre4/src/onnx_models_new/RecoTauTag-TrainingFiles/DeepTauId/deepTau_2017v2p6_e6_core.onnx"});
+				       {"/afs/cern.ch/work/p/ppalit2/public/test_sonic/CMSSW_16_0_0_pre4/src/onnx_models_new/RecoTauTag-TrainingFiles/DeepTauId/deepTau_2017v2p6_e6_core.onnx"});
     //desc.add<bool>("mem_mapped", false);
     descriptions.add("DeepTauONNX", desc);
   }
-
+  
   explicit DeepTauIdONNX(const edm::ParameterSet& cfg,
-                          const deep_tau::DeepTauCacheONNX* cache)
-      : DeepTauIdBaseONNX<DeepTauIdONNXWrapper>(cfg), cache_(cache) {
-
+			 const deep_tau::DeepTauCacheONNX* cache)
+    : DeepTauIdBaseONNX<DeepTauIdONNXWrapper>(cfg), cache_(cache) {
+    
     if (version_ != 2)
       throw cms::Exception("DeepTauIdONNX")
-          << "version " << version_ << " is not supported.";
-
+	<< "version " << version_ << " is not supported.";
+    
     using namespace dnn_inputs_v2;
-
+    
     if (sub_version_ == 1) {
       tauBlockSize_ = TauBlockInputs::NumberOfInputs;
     } else if ((sub_version_ == 5) || ((sub_version_ == 0) && (year_ == 20161718))) {
       tauBlockSize_ = static_cast<int>(TauBlockInputs::NumberOfInputs) -
-                      static_cast<int>(TauBlockInputs::varsToDrop.size());
+	static_cast<int>(TauBlockInputs::varsToDrop.size());
     } else {
       throw cms::Exception("DeepTauIdONNX")
-          << "sub_version " << sub_version_ << " is not supported.";
+	<< "sub_version " << sub_version_ << " is not supported.";
     }
-
+    
     tauBlock_.resize(tauBlockSize_, 0.f);
-
+    
     for (size_t n = 0; n < 2; ++n) {
       const bool is_inner = (n == 0);
       const int n_cells = is_inner ? number_of_inner_cell : number_of_outer_cell;
       const int max_cells = n_cells * n_cells;
-
+      
+      // get zero placeholder from network with 1 zero cell
+      eGammaBlock_[is_inner].assign(EgammaBlockInputs::NumberOfInputs, 0.f);
+      muonBlock_[is_inner].assign(MuonBlockInputs::NumberOfInputs, 0.f);
+      hadronBlock_[is_inner].assign(HadronBlockInputs::NumberOfInputs, 0.f);
+      zeroConvFeatures_[is_inner] = getPartialPredictions(is_inner, 1);
+      
+      // now assign full size for event processing
       eGammaBlock_[is_inner].assign(max_cells * EgammaBlockInputs::NumberOfInputs, 0.f);
       muonBlock_[is_inner].assign(max_cells * MuonBlockInputs::NumberOfInputs, 0.f);
       hadronBlock_[is_inner].assign(max_cells * HadronBlockInputs::NumberOfInputs, 0.f);
       convFeatures_[is_inner].assign(n_cells * n_cells * number_of_conv_features, 0.f);
-      zeroConvFeatures_[is_inner].assign(number_of_conv_features, 0.f);
-    }
-  }
+      
+      /*
+      eGammaBlock_[is_inner].assign(max_cells * EgammaBlockInputs::NumberOfInputs, 0.f);
+      muonBlock_[is_inner].assign(max_cells * MuonBlockInputs::NumberOfInputs, 0.f);
+      hadronBlock_[is_inner].assign(max_cells * HadronBlockInputs::NumberOfInputs, 0.f);
+      convFeatures_[is_inner].assign(n_cells * n_cells * number_of_conv_features, 0.f);
 
+      eGammaBlock_[is_inner].assign(EgammaBlockInputs::NumberOfInputs, 0.f);
+      muonBlock_[is_inner].assign(MuonBlockInputs::NumberOfInputs, 0.f);
+      hadronBlock_[is_inner].assign(HadronBlockInputs::NumberOfInputs, 0.f);
+      zeroConvFeatures_[is_inner] = getPartialPredictions(is_inner, 1);
+      //zeroConvFeatures_[is_inner].assign(number_of_conv_features, 0.f);
+
+      // restore to full size for event processing
+      eGammaBlock_[is_inner].assign(max_cells * EgammaBlockInputs::NumberOfInputs, 0.f);
+      muonBlock_[is_inner].assign(max_cells * MuonBlockInputs::NumberOfInputs, 0.f);
+      hadronBlock_[is_inner].assign(max_cells * HadronBlockInputs::NumberOfInputs, 0.f);
+      */
+      }
+  }
+  
   static std::unique_ptr<deep_tau::DeepTauCacheONNX>
   initializeGlobalCache(const edm::ParameterSet& cfg) {
     const auto graph_name_vector = cfg.getParameter<std::vector<std::string>>("graph_file");
@@ -107,13 +131,13 @@ public:
     }
     return std::make_unique<deep_tau::DeepTauCacheONNX>(graph_names);
   }
-
+  
   static void globalEndJob(const deep_tau::DeepTauCacheONNX*) {}
-
+  
   void produce(edm::Event& event, const edm::EventSetup& es) override {
     edm::Handle<TauCollection> taus;
     event.getByToken(tausToken_, taus);
-
+    
     //std::cout << "!!!!!!!!!!!!!!!!!!!!!!!Here is producer!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
     
     if (taus->empty()) {
@@ -121,14 +145,14 @@ public:
       createOutputs(event, emptyPred, taus);
       return;
     }
-
+    
     loadPrediscriminants(event, taus);
     const auto pred = getPredictions(event, taus);
     createOutputs(event, pred, taus);
   }
-
+  
 private:
-
+  
   void setCellConvFeatures(bool is_inner, int eta_index, int phi_index,
                            const float* features_ptr) {
     using namespace dnn_inputs_v2;
@@ -175,6 +199,22 @@ private:
     auto outputs = cache_->getSession(sess_name).run(
         input_names, input_values, input_shapes, output_names);
 
+    // debug diff
+    /*
+    float sum = 0.f;
+    for (auto v : outputs.at(0)) sum += v;
+    std::cout << "ONNX PARTIAL PRED is_inner=" << is_inner << " n_valid_cells=" << n_valid_cells << " sum=" << sum << std::endl;
+    */
+
+    //debug diff v2
+    /*
+    std::cout << "PARTIAL size=" << outputs.at(0).size() 
+              << " n_valid_cells=" << n_valid_cells 
+              << " expected=" << n_valid_cells * dnn_inputs_v2::number_of_conv_features 
+              << std::endl;
+    //
+    */
+    
     return outputs.at(0);
   }
 
@@ -204,6 +244,10 @@ private:
         for (int phi = -grid.maxPhiIndex(); phi <= grid.maxPhiIndex(); ++phi) {
           const CellIndex cell_index{eta, phi};
           if (grid.find(cell_index) == grid.end()) continue;
+
+	  //debug diff v3
+	  //std::cout << "FILL idx=" << idx << " eta=" << eta << " phi=" << phi << std::endl;
+	  
           const Cell& cell = grid.find(cell_index)->second;
 
           auto eg_it  = eGammaBlock_[is_inner].begin() + idx * EgammaBlockInputs::NumberOfInputs;
@@ -225,6 +269,14 @@ private:
 
       const std::vector<float> partial = getPartialPredictions(is_inner, n_valid);
 
+      //debug diff v4
+      /*
+      float sum_partial = 0.f;
+      for (auto v : partial) sum_partial += v;
+      std::cout << "PARTIAL SUM inside createConvFeatures is_inner=" << is_inner 
+		<< " sum=" << sum_partial << std::endl;
+      */
+
       unsigned idx2 = 0;
       for (int eta = -grid.maxEtaIndex(); eta <= grid.maxEtaIndex(); ++eta) {
         for (int phi = -grid.maxPhiIndex(); phi <= grid.maxPhiIndex(); ++phi) {
@@ -232,6 +284,11 @@ private:
           const int eta_idx = grid.getEtaTensorIndex(cell_index);
           const int phi_idx = grid.getPhiTensorIndex(cell_index);
           if (grid.find(cell_index) != grid.end()) {
+
+	    // debug diff v3
+	    //std::cout << "SCATTER idx2=" << idx2 << " eta=" << eta << " phi=" << phi 
+            //              << " eta_index=" << eta_idx << " phi_index=" << phi_idx << std::endl;
+	    
             setCellConvFeatures(is_inner, eta_idx, phi_idx,
                                 &partial[idx2 * number_of_conv_features]);
             ++idx2;
@@ -241,6 +298,15 @@ private:
           }
         }
       }
+
+      //debug diff v4
+      /*
+      float sum_conv = 0.f;
+      for (auto v : convFeatures_[is_inner]) sum_conv += v;
+      std::cout << "CONV SUM after scatter is_inner=" << is_inner 
+		<< " sum=" << sum_conv << std::endl;
+      */
+
     } else {
       const int n_cells = is_inner ? number_of_inner_cell : number_of_outer_cell;
       convFeatures_[is_inner].assign(n_cells * n_cells * number_of_conv_features, 0.f);
@@ -259,52 +325,63 @@ private:
                                       const edm::EventNumber_t& eventnr,
                                       TauFunc tau_funcs) {
     using namespace dnn_inputs_v2;
-
+    
     CellGrid inner_grid(number_of_inner_cell, number_of_inner_cell, 0.02, 0.02, disable_CellIndex_workaround_);
     CellGrid outer_grid(number_of_outer_cell, number_of_outer_cell, 0.05, 0.05, disable_CellIndex_workaround_);
-
+    
     fillGrids(dynamic_cast<const TauCastType&>(tau), *electrons, inner_grid, outer_grid);
     fillGrids(dynamic_cast<const TauCastType&>(tau), *muons, inner_grid, outer_grid);
     fillGrids(dynamic_cast<const TauCastType&>(tau), pfCands, inner_grid, outer_grid);
-
+    
     std::fill(tauBlock_.begin(), tauBlock_.end(), 0.f);
     auto tau_it = tauBlock_.begin();
     createTauBlockInputs<CandidateCastType>(
-        dynamic_cast<const TauCastType&>(tau),
-        tau_index, tau_ref, pv, rho, tau_funcs, tau_it);
-
+					    dynamic_cast<const TauCastType&>(tau),
+					    tau_index, tau_ref, pv, rho, tau_funcs, tau_it);
+    
     createConvFeatures<CandidateCastType>(
-        dynamic_cast<const TauCastType&>(tau),
-        tau_index, tau_ref, pv, rho,
-        electrons, muons, pfCands, inner_grid, tau_funcs, true);
+					  dynamic_cast<const TauCastType&>(tau),
+					  tau_index, tau_ref, pv, rho,
+					  electrons, muons, pfCands, inner_grid, tau_funcs, true);
     createConvFeatures<CandidateCastType>(
-        dynamic_cast<const TauCastType&>(tau),
-        tau_index, tau_ref, pv, rho,
-        electrons, muons, pfCands, outer_grid, tau_funcs, false);
-
+					  dynamic_cast<const TauCastType&>(tau),
+					  tau_index, tau_ref, pv, rho,
+					  electrons, muons, pfCands, outer_grid, tau_funcs, false);
+    
     using namespace dnn_inputs_v2;
     const int64_t ni = number_of_inner_cell;
     const int64_t no = number_of_outer_cell;
     const int64_t nf = number_of_conv_features;
-
+    
     std::vector<std::string> input_names = {"input_tau", "input_inner", "input_outer"};
     std::vector<std::vector<float>> input_values = {
-        tauBlock_,
-        convFeatures_[true],
-        convFeatures_[false]
+      tauBlock_,
+      convFeatures_[true],
+      convFeatures_[false]
     };
     std::vector<std::vector<int64_t>> input_shapes = {
-        {1, tauBlockSize_},
-        {1, ni, ni, nf},
-        {1, no, no, nf}
+      {1, tauBlockSize_},
+      {1, ni, ni, nf},
+      {1, no, no, nf}
     };
     const std::vector<std::string> output_names = {"main_output/Softmax"};
 
+    // debug for diff
+    /*
+    float sum_inner = 0.f, sum_outer = 0.f;
+    for (auto v : convFeatures_[true])  sum_inner += v;
+    for (auto v : convFeatures_[false]) sum_outer += v;
+    std::cout << "CONV FEATURES sum_inner=" << sum_inner << " sum_outer=" << sum_outer << std::endl;
+    //
+    */
+    
     auto outputs = cache_->getSession("core").run(
-        input_names, input_values, input_shapes, output_names);
-
+						  input_names, input_values, input_shapes, output_names);
+    
     std::vector<float> pred = outputs.at(0);
 
+    //std::cout << "*****************ONNX RAW PRED tau_index=" << tau_index << " e=" << pred[0] << " mu=" << pred[1] << " tau=" << pred[2] << " jet=" << pred[3] << std::endl;
+    
     if (debug_level >= 1) {
       static const std::array<std::string, 4> labels = {"e", "mu", "tau", "jet"};
       std::cout << "output = { ";
@@ -315,9 +392,87 @@ private:
       std::cout << " }" << std::endl;
     }
 
+    /*
+    if (save_inputs_) {
+      std::string json_file_name = "DeepTauIdONNX_" + std::to_string(eventnr) + "_" + std::to_string(tau_index) + ".json";
+      std::ofstream jf(json_file_name);
+      jf << "{";
+      
+      // tau block
+      jf << "\"input_tau\": [";
+      for (int i = 0; i < tauBlockSize_; ++i) {
+        if (i) jf << ", ";
+        jf << tauBlock_[i];
+      }
+      jf << "]";
+      
+      jf << "}";
+      }*/
+
+    if (save_inputs_) {
+      std::string json_file_name = "DeepTauIdONNX_" + std::to_string(eventnr) + "_" + std::to_string(tau_index) + ".json";
+      std::ofstream jf(json_file_name);
+      jf << "{";
+      
+      // tau block
+      jf << "\"input_tau\": [";
+      for (int i = 0; i < tauBlockSize_; ++i) {
+        if (i) jf << ", ";
+        jf << tauBlock_[i];
+      }
+      jf << "]";
+      
+      // helper lambda to dump a grid block
+      auto dumpGrid = [&](const std::string& block_name,
+			  const std::vector<float>& buf,
+			  int n_inputs,
+			  const CellGrid& grid) {
+        jf << ", \"" << block_name << "\": [";
+        const int n_eta = grid.maxEtaIndex();
+        const int n_phi = grid.maxPhiIndex();
+        int idx = 0;
+        for (int eta = -n_eta; eta <= n_eta; ++eta) {
+	  if (eta != -n_eta) jf << ", ";
+	  jf << "[";
+	  for (int phi = -n_phi; phi <= n_phi; ++phi) {
+	    if (phi != -n_phi) jf << ", ";
+	    jf << "[";
+	    const CellIndex ci{eta, phi};
+	    const auto it = grid.find(ci);
+	    for (int f = 0; f < n_inputs; ++f) {
+	      if (f) jf << ", ";
+	      float v = 0.f;
+	      if (it != grid.end())
+		v = buf[idx * n_inputs + f];
+	      jf << v;
+	    }
+	    if (it != grid.end()) ++idx;
+	    jf << "]";
+	  }
+	  jf << "]";
+        }
+        jf << "]";
+      };
+      
+      dumpGrid("input_inner_egamma", eGammaBlock_[true],
+	       dnn_inputs_v2::EgammaBlockInputs::NumberOfInputs, inner_grid);
+      dumpGrid("input_inner_muon", muonBlock_[true],
+	       dnn_inputs_v2::MuonBlockInputs::NumberOfInputs, inner_grid);
+      dumpGrid("input_inner_hadrons", hadronBlock_[true],
+	       dnn_inputs_v2::HadronBlockInputs::NumberOfInputs, inner_grid);
+      dumpGrid("input_outer_egamma", eGammaBlock_[false],
+	       dnn_inputs_v2::EgammaBlockInputs::NumberOfInputs, outer_grid);
+      dumpGrid("input_outer_muon", muonBlock_[false],
+	       dnn_inputs_v2::MuonBlockInputs::NumberOfInputs, outer_grid);
+      dumpGrid("input_outer_hadrons", hadronBlock_[false],
+	       dnn_inputs_v2::HadronBlockInputs::NumberOfInputs, outer_grid);
+      
+      jf << "}";
+    }
+    
     return pred;
   }
-
+  
   std::vector<std::vector<float>> getPredictions(edm::Event& event,
                                                   edm::Handle<TauCollection> taus) {
     const std::vector<pat::Electron> electron_collection_default;
